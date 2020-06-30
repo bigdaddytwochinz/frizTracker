e944b9085180cd2d4e46b9e01d750b79682690fd
Achinth Bharadwaj <achinth.bharadwaj@gmail.com>
2020-06-30 01:31:57 -0700

    added vue routes to hhome and about

---
+++ vue-frontend/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div>
+      <p>Well, well, well. This is who we are.</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'About.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-frontend/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p> Welcome to the frizTracker.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
